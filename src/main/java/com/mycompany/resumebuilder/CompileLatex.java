cf745279f127d695ff730cd119a76709da6e0324
eden-chung <edenchung23@gmail.com>
2023-07-26 23:46:48 +0200

    initial commit

---
+++ src/main/java/com/mycompany/resumebuilder/CompileLatex.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.resumebuilder;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+/**
+ *
+ * @author EdenChung
+ */
+public class CompileLatex {
+    
+    private static final String RESUME_PATH = "/Users/EdenChung/Desktop/Eden/Home/Coding/Java/ResumeBuilder/src/main/java/com/mycompany/resumebuilder/resume.tex";
+    
+    public void resetTemplate() {
+        String pathToBackupFile = "/Users/EdenChung/Desktop/Eden/Home/Coding/Java/ResumeBuilder/src/main/java/com/mycompany/resumebuilder/resume_orig.tex";
+        String pathToTexFile = "/Users/EdenChung/Desktop/Eden/Home/Coding/Java/ResumeBuilder/src/main/java/com/mycompany/resumebuilder/resume.tex";
+
+        try {
+            Path backupPath = Paths.get(pathToBackupFile);
+            Path texPath = Paths.get(pathToTexFile);
+
+            // Copy the contents of the backup file to the .tex file
+            Files.copy(backupPath, texPath, StandardCopyOption.REPLACE_EXISTING);
+
+            System.out.println("Template reset successful.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Template reset failed.");
+        }
+    }
+    
+    public static void compileFile() {
+        try {
+            String pathToFile = "/Users/EdenChung/Desktop/Eden/Home/Coding/Java/ResumeBuilder/src/main/java/com/mycompany/resumebuilder/resume.tex"; // Replace with the path to your .tex file
+            String pdflatexPath = "/Library/TeX/texbin/pdflatex";
+            
+            String outputDirectory = "/Users/EdenChung/Desktop/Eden/Home/Coding/Java/ResumeBuilder/src/main/java/com/mycompany/resumebuilder/";
+                    
+            String[] command = {pdflatexPath, "-interaction=nonstopmode", "-output-directory=" + outputDirectory, pathToFile};
+            
+            String currentDir = System.getProperty("user.dir");
+            System.out.println("Current Working Directory: " + currentDir);
+            
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+            processBuilder.redirectErrorStream(true);
+
+            Process process = processBuilder.start();
+            
+
+
+            // Wait for the process to finish
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                System.out.println("Compilation successful.");
+            } else {
+                System.out.println("Compilation failed.");
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void addName(String name) {
+        try {
+            // Read the .tex template file
+            String templateContent = new String(Files.readAllBytes(Paths.get(RESUME_PATH)));
+
+            // Replace placeholders with user input
+            String resumeContent = templateContent.replace("{name}", name);
+
+            // Save the modified content back to the .tex file
+            Files.write(Paths.get(RESUME_PATH), resumeContent.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void addSchool(String school) {
+        try {
+            // Read the .tex template file
+            String templateContent = new String(Files.readAllBytes(Paths.get(RESUME_PATH)));
+
+            // Replace placeholders with user input
+            String resumeContent = templateContent.replace("{school}", school);
+
+            // Save the modified content back to the .tex file
+            Files.write(Paths.get(RESUME_PATH), resumeContent.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+
+    
+}
+
---
+++ src/main/java/com/mycompany/resumebuilder/ResumeBuilder.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+package com.mycompany.resumebuilder;
+
+import javax.swing.*;
+import java.io.*;
+
+        
+/**
+ *
+ * @author EdenChung
+ */
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ResumeBuilder implements NewJFrame.InputListener {
+    
+    private CompileLatex latex = new CompileLatex();
+    
+    public void onInputSubmitted(String name, String school) {
+        System.out.println("Name: " + name);
+        System.out.println("School: " + school);
+
+        // Once you have the input data, you can proceed with compiling LaTeX or any other tasks
+        latex.resetTemplate();
+        latex.addName(name);
+        latex.addSchool(school);
+        latex.compileFile();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        //ResumeBuilder builder = new ResumeBuilder();
+        
+        //builder.latex.resetTemplate();
+        
+        NewJFrame frame1 = new NewJFrame();
+        frame1.setInputListener(new ResumeBuilder());
+        SwingUtilities.invokeLater(() -> frame1.setVisible(true));
+        
+
+    }
+    
+    
+}
